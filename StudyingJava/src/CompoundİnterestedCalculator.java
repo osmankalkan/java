422241771a54b7ed8485dfc18e839a2e0cd02446
Osman Kalkan <okalkan@posta.my.edu.tr>
2025-03-02 17:01:34 +0300

    new

--- StudyingJava/src/CompoundİnterestedCalculator.java
+++ StudyingJava/src/CompoundİnterestedCalculator.java
@@ -7,6 +7,31 @@ public class CompoundİnterestedCalculator {
         Scanner scanner = new Scanner(System.in);
 
         double principal;
+        double rate;
+        int timesCompounded;
+        int years ;
+        double amount;
+
+
+        System.out.print("Enter the principal amount; ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the internet rate (in%):  ");
+        rate = scanner.nextDouble()/ 100;
+
+        System.out.print("enter the # of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompounded ,timesCompounded * years);
+
+        System.out.printf("The amount after %d years is %.2f" , years,amount);
+
+
+
+        scanner.close();
 
 
 
