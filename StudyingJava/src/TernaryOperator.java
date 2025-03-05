07604567d23b8b99041f6f4ee3f0a8795ab4d0a6
Osman Kalkan <okalkan@posta.my.edu.tr>
2025-03-05 17:32:47 +0300

    new

---
+++ StudyingJava/src/TernaryOperator.java
@@ -0,0 +1,31 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+
+        //ternary operator ? = Return 1 of 2 values if a condition true
+
+
+        /*
+        int score =60;
+
+        String passOrFail = (score>=65) ? "PASS " : "FAIL";
+        System.out.println(passOrFail);
+
+
+
+
+        int number =3;
+
+        String evenOrOdd = (number % 2==0) ? "even" : "odd";
+        System.out.println(evenOrOdd);
+
+         */
+
+        int hours = 13;
+
+        String timeOdDay = (hours < 12) ? "A.M" : "P.M";
+
+        System.out.println(timeOdDay);
+
+    }
+}
--- StudyingJava/src/WeightConverter.java
+++ StudyingJava/src/WeightConverter.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class WeightConverter {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        double weight;
+        double newWeight;
+        int choice;
+
+        System.out.println("Weight conversion program");
+        System.out.println("1:convert lbs to kgs");
+        System.out.println("2:convert kgs to lbs");
+
+        System.out.print("Choose an option:");
+        choice= scanner.nextInt();
+
+        if (choice == 1){
+            System.out.print("Enter the weight in lbs: ");
+            weight=scanner.nextDouble();
+            newWeight= weight*0.453592;
+            System.out.printf("The new weight in kgs is: %.2f", newWeight);
+        }
+        else if (choice==2){
+            System.out.print("Enter the weight in kgs : ");
+            weight=scanner.nextDouble();
+            newWeight=weight * 2.20462;
+            System.out.printf("The new weight in lbs is: %.2f",newWeight);
+        }
+        else {
+            System.out.println("That was not a valid choice");
+        }
+
+        scanner.close();
+
+
+
+    }
 }
