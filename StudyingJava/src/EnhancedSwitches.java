4395b2ba7ed45ac3f44dfff4a78be66011ca712f
Osman Kalkan <okalkan@posta.my.edu.tr>
2025-03-06 19:53:11 +0300

    new

---
+++ StudyingJava/src/EnhancedSwitches.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class EnhancedSwitches {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the day of week: ");
+        String day = scanner.nextLine();
+
+
+/*
+        switch (day){
+            case "monday" -> System.out.println("it is a weekday ");
+            case "tuesday" -> System.out.println("it is a weekday ");
+            case "thursday" -> System.out.println("it is a weekday ");
+            case "wednesday" -> System.out.println("it is  a weekday ");
+            case "friday" -> System.out.println("it is a weekday ");
+            case "saturday" -> System.out.println("it is the weekend ");
+            case "sunday" -> System.out.println("it is the weekend ");
+            default -> System.out.println("is not a day");
+
+ */
+
+
+            switch (day){
+                case "monday", "tuesday"," wednesday" ,"thursday", "friday"-> System.out.println("it is a weekday");
+                case "saturday","sunday" -> System.out.println("it is the weekend");
+
+                default -> System.out.println(day +"is not a day");
+        }
+    }
+}
