0e11be17dfe984e2fbe4418758c2b8d63e2c7cfc
Osman Kalkan <okalkan@posta.my.edu.tr>
2025-03-22 16:41:49 +0300

    new

--- StudyingJava/src/NımberGuessingGame.java
+++ StudyingJava/src/NımberGuessingGame.java
@@ -1,5 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class NımberGuessingGame {
     public static void main(String[] args) {
 
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts=0;
+        int min = 1;
+        int max = 100;
+        int randomNumber= random.nextInt(min,max + 1 );
+
+
+        System.out.println("Number Guessing Game");
+        System.out.printf("Guess a number between %d-%d\n",min , max);
+
+        do {
+            System.out.print("Enter a guess :");
+            guess = scanner.nextInt();
+            attempts++;
+            
+            if (guess< randomNumber){
+                System.out.println("TOO LOW! Try Again");
+            } else if (guess>randomNumber) {
+                System.out.println("TOO HİGH! Try Again");
+
+            }
+            else{
+                System.out.println("CORRECT! THE NUMBER WAS " + randomNumber);
+                System.out.println("# of attempts: " + attempts);
+            }
+
+        }while (guess != randomNumber);
+
+        scanner.close();
+
+
+        
     }
 }
